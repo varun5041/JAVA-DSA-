480f1ab5741ae426065b5552ecf242c6e1771ca5
VARUN SONI <varunsoni21102005@gmail.com>
2025-08-06 18:35:13 +0530

    Add files via upload

--- subarraysprint.java
+++ subarraysprint.java
@@ -1,3 +1,25 @@
 public class subarraysprint {
-    
+    public static void main(String args[]){
+        int a[]={2,4,3,9,7,6};
+        for(int i=0;i<=a.length-1;i++){
+            System.out.println("subarrays of" + a[i] + ":");
+             if(i==a.length-1){
+                        System.out.println("[" + a[i] + "]");
+                    }
+            for(int j=i+1;j<=a.length-1;j++){
+                System.out.print("[");
+                for(int k=i;k<=j;k++){
+                
+                    System.out.print(a[k]);
+                    if(k!=j){
+                        System.out.print(",");
+                    }
+                    
+
+                }
+                System.out.println("]");
+            }
+        }
+       
+    }
 }
