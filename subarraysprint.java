0330847a48c0400d51464f7538270e8b847db268
VARUN SONI <varunsoni21102005@gmail.com>
2025-08-06 18:47:49 +0530

    subarraysprint.java

--- subarraysprint.java
+++ subarraysprint.java
@@ -2,22 +2,30 @@ public class subarraysprint {
     public static void main(String args[]){
         int a[]={2,4,3,9,7,6};
         for(int i=0;i<=a.length-1;i++){
+            int sum=0;
             System.out.println("subarrays of" + a[i] + ":");
              if(i==a.length-1){
                         System.out.println("[" + a[i] + "]");
+                        System.out.print(" SUM:" + a[i]);
                     }
             for(int j=i+1;j<=a.length-1;j++){
                 System.out.print("[");
                 for(int k=i;k<=j;k++){
                 
                     System.out.print(a[k]);
+                    sum=sum+a[k];
+                    
                     if(k!=j){
                         System.out.print(",");
+                        
                     }
                     
 
                 }
-                System.out.println("]");
+                System.out.print("]");
+                System.out.print("SUM:" + sum);
+                System.out.println();
+                sum=0;
             }
         }
        
